ae2ba4333fc2af11eb42fb7b952ece7051ed17f9
Davina Engelmann <dengelma@1-B-10.42heilbronn.de>
2022-05-10 12:59:58 +0200

    merge

--- src/new_project.c
+++ src/new_project.c
@@ -1,10 +1,10 @@
 #include "../inc/cub3d.h"
 
-
 int	main(void)
 {
 	ft_putstr_fd("hello\n", 2);
 	// asfkljasdfiagi
 	// merge conflict des todes
+	ft_putstr_fd("hello davina\n", 2);
 	return (0);
 }
